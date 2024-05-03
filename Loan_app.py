7be301dcd2275f62178f80d23f3fdbe43ae10998
Muhammed Rashid <103042046+Rashid9226@users.noreply.github.com>
2024-05-03 16:48:20 +0530

    Add files via upload

---
+++ Loan_app.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import streamlit as st
+from sklearn.ensemble import RandomForestClassifier
+
+
+def user_input_features():
+    with st.form("penguin_form"):
+        ApplicantIncome=st.slider("Applicant Income",min_value=150,max_value=10500,value=5000)
+        CoapplicantIncome = st.slider("CoapplicantIncome", min_value=150, max_value=6000, value=500)
+        LoanAmount = st.slider("LoanAmount", min_value=0, max_value=500, value=250)
+        Loan_Amount_Term = st.slider("Loan_Amount_Term", min_value=0, max_value=500, value=250)
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {
+            "ApplicantIncome" : ApplicantIncome,
+            "CoapplicantIncome" : CoapplicantIncome,
+            "LoanAmount" : LoanAmount,
+            "Loan_Amount_Term" : Loan_Amount_Term
+        }
+        features = pd.DataFrame(data,index = [0])
+        return features
+    else:
+        return None
+
+df=user_input_features()
+
+if df is not None:
+    st.subheader('User Input Parameters')
+    st.write(df)
+
+    Loan = pd.read_csv("Loan_CData.csv")
+    X = Loan[["ApplicantIncome", "CoapplicantIncome", "LoanAmount", "Loan_Amount_Term"]]
+    y = Loan["Loan_Status"]
+
+    clf = RandomForestClassifier()
+    clf.fit(X, y)
+    prediction = clf.predict(df)
+    prediction_prob = clf.predict_proba(df)
+
+    st.subheader('Prediction')
+    st.write(prediction)
+
+    st.subheader('Prediction Probability')
+    st.write(prediction_prob)
